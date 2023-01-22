854036c11f3a74fc99b761d85e2175605c15c4fa
yashdkadam <kadamdyash55@gmail.com>
2023-01-22 16:54:05 +0530

    initial commit

--- search/main.js
+++ search/main.js
@@ -1,11 +1,11 @@
 if (localStorage.getItem("time") === null) {
   localStorage.setItem("time", "day");
 }
-function appendHtml(data, time) {
+function appendHtml(data) {
   var body = "";
   for (let i = 0; i < data.length; i++) {
     if (data[i]["magnet"] != undefined) {
-      body += `<a href='./player' onclick='setLocal("${data[i]["magnet"]}")' style="text-decoration: none;">
+      body += `<a href='../player' onclick='setLocal("${data[i]["magnet"]}")' style="text-decoration: none;">
           <div class="card m-3" style="width: 18rem;">
             <img src="https://i.im.ge/2023/01/22/sRNDMY.placeholder.jpg" class="card-img-top" alt="...">
             <div class="card-body">
@@ -13,7 +13,7 @@ function appendHtml(data, time) {
             <p class="card-text">${data[i].name}</p>
             <p class="card-text">Seeders : ${data[i].seeder}</p>
             <p class="card-text">Lechers : ${data[i].leecher}</p>
-            <a href="./player" class="btn btn-primary">Stream Now</a>
+            <a href="../player" class="btn btn-primary">Stream Now</a>
             </div>
             </div>
             </a>`;
@@ -21,25 +21,18 @@ function appendHtml(data, time) {
   }
   var html = `<div class="contain">`;
   html += body + `</div>`;
-  // localStorage.setItem("html", html);
   $(document).ready(function () {
-    // var html = localStorage.getItem("html");
     $("body").append(html);
-    // $(".day").slick({
-    //   infinite: true,
-    //   slidesToShow: 1,
-    //   slidesToScroll: 1,
-    // });
   });
 }
 async function get() {
-  var time = localStorage.getItem("time");
+  var search = localStorage.getItem("search");
   var url =
-    "https://corsproxy.io/?https://curious-parka-yak.cyclic.app/api/torrents/" +
-    time;
+    "https://corsproxy.io/?https://curious-parka-yak.cyclic.app/api/torrents/search/" +
+    search;
   const response = await fetch(url).then((d) => d.json());
   console.log(response);
-  appendHtml(response, time);
+  appendHtml(response);
 }
 get();
 
